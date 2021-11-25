0b965d960a1eeecc3518e9aaf0ab39e6e2ddeb69
Chetan <you@example.comcg.narkhede@gmail.com>
2021-11-25 22:45:17 +0530

    First commit

---
+++ src/test/java/Test_Package/MyClass.java
@@ -0,0 +1,16 @@
+package Test_Package;
+
+import org.testng.annotations.Test;
+
+public class MyClass {
+
+	@Test
+	public void A() {
+		System.out.println("A");
+	}
+	
+	@Test
+	public void B() {
+		System.out.println("B");
+	}
+}
---
+++ src/test/java/Test_Package/OtherClass.java
@@ -0,0 +1,16 @@
+package Test_Package;
+
+import org.testng.annotations.Test;
+
+public class OtherClass {
+
+	@Test
+	public void C() {
+		System.out.println("C");
+	}
+	
+	@Test
+	public void D() {
+		System.out.println("D");
+	}
+}
